9af625f0c6e5a5ac0cd6a04119f05639a4b952b1
Floris Heyvaert <floris.heyvaert@gmail.com>
2021-02-13 02:40:11 +0100

    repo

---
+++ src/Blace.Editor/Editing/AceRepository.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Blace.Editor.Editing
+{
+    public class AceRepository
+    {
+        private string _location;
+
+        public AceRepository()
+        {
+            _location = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "wwwroot", "ace");
+        }
+
+        public async Task<List<string>> GetThemes()
+        {
+            var result = GetLocationWithRegex("theme-");
+            var themes = new List<string>();
+            foreach (var theme in result)
+                themes.Add($"ace/theme/{theme}");
+            return themes;
+        }
+
+        public async Task<List<string>> GetModes()
+        {
+            var result = GetLocationWithRegex("mode-");
+            var modes = new List<string>();
+            foreach (var mode in result)
+                modes.Add($"ace/mode/{mode}");
+            return modes;
+        }
+
+        private List<string> GetLocationWithRegex(string pattern)
+        {
+            var files = Directory.GetFiles(_location);
+            var result = new List<string>();
+            foreach (var file in files)
+            {
+                var regex = new Regex($"(?<={pattern}).*?(?=\\.js)");
+                var match = regex.Match(file);
+                if (match.Success)
+                    result.Add(match.Value);
+            }
+            return result;
+        }
+    }
+}

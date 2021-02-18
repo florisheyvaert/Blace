4e7c80f1259cc19f8c16e7ac43e0f07bc94bdd85
Floris Heyvaert <floris.heyvaert@gmail.com>
2021-02-18 11:22:25 +0100

    moved tab to component

---
+++ src/Blace/Components/EditorTab.razor.cs
@@ -0,0 +1,18 @@
+﻿using Blace.Editing;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blace.Components
+{
+    public class EditorTabBase : ComponentBase
+    {
+        [Parameter] public BaseEditorFile File { get; set; }
+        [Parameter] public BaseEditorFile SelectedFile { get; set; }
+        [Parameter] public EventCallback<BaseEditorFile> CloseFileClicked { get; set; }
+        [Parameter] public EventCallback<BaseEditorFile> OpenFileClicked { get; set; }
+    }
+}
